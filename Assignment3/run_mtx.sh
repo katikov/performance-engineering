24b7ae0b6385d7b78fc8659a4b48bdf9a222720a
XU-Nuo <xunuo_promise@outlook.com>
2022-05-22 08:39:12 +0200

    Fix parameter passing

--- Assignment3/run_mtx.sh
+++ Assignment3/run_mtx.sh
@@ -5,16 +5,16 @@ make
 N_EXP=30
 
 # Count the matrix
-N_MTX=$(ls ../data | wc -l)
+N_MTX=$(ls data | wc -l)
 APP_NAME=${PWD##*/}
 echo "===${APP_NAME}==="
 echo "${N_MTX} matrix to measure"
-for d in $(find "../data" -maxdepth 1 -type f)
+for d in $(find "data" -maxdepth 1 -type f)
 do 
     for ((i=1;i<=N_EXP;i++));
     do
-        prun -np 1 -reserve $1 ./coo_spmv $d tmp
-        prun -np 1 -reserve $1 ./csr_spmv $d tmp
+        prun -np 1 -reserve $1 ./coo_spmv -f $d -o tmp
+        prun -np 1 -reserve $1 ./csr_spmv -f $d -o tmp
         # TIME=$(./matmul $d tmp.mtx result.mtx)
         # echo $MTX,$1,$TIME
     done
